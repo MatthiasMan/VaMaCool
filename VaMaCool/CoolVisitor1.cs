de7706521795a714d97b7358ab9d1ccaa9088fe3
marth98 <marth_mandl@gmx.at>
2022-12-11 16:11:51 +0100

    Add project files.

---
+++ VaMaCool/CoolVisitor1.cs
@@ -0,0 +1,75 @@
+﻿using Antlr4.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VaMaCool
+{
+    public class CoolVisitor1 : CoolBaseVisitor<int>
+    {
+        public override int VisitClassDefine([NotNull] CoolParser.ClassDefineContext context)
+        {
+            return base.VisitClassDefine(context);
+        }
+
+        public override int VisitMethod([NotNull] CoolParser.MethodContext context)
+        {
+            return base.VisitMethod(context);
+        }
+
+        public override int VisitExpression([NotNull] CoolParser.ExpressionContext context)
+        {
+            return base.VisitExpression(context);
+        }
+
+        public override int VisitId([NotNull] CoolParser.IdContext context)
+        {
+            return base.VisitId(context);
+        }
+
+        public override int VisitInt([NotNull] CoolParser.IntContext context)
+        {
+            Console.WriteLine("--VisitInt--");
+            int.TryParse(context.INT().GetText(), out int value);
+            return value;
+        }
+
+        public override int VisitParentheses([NotNull] CoolParser.ParenthesesContext context)
+        {
+            return base.VisitParentheses(context);
+        }
+
+        public override int VisitArithmetic([NotNull] CoolParser.ArithmeticContext context)
+        {
+            Console.WriteLine("--VisitArithmetic--");
+            int value = 0;
+
+            int left = Visit(context.expression(0));
+            int right = Visit(context.expression(1));
+            switch (context.op.Text)
+            {
+                case "+":
+                    value = left + right;
+                    break;
+                case "-":
+                    value = left - right;
+                    break;
+                case "*":
+                    value = left * right;
+                    break;
+                case "/":
+                    value = left / right;
+                    break;
+                default:
+                    throw new Exception("Arithmetic operator not found");
+            }
+            return value;
+        }
+
+        public override int VisitAssignment([NotNull] CoolParser.AssignmentContext context)
+        {
+            Console.WriteLine("--VisitAssignment--");
+            return base.VisitAssignment(context);
+        }
+    }
+}
---
+++ VaMaCool/Program.cs
@@ -0,0 +1,61 @@
+﻿using Antlr4.Runtime;
+using System;
+using System.Collections.Generic;
+
+namespace VaMaCool
+{
+    class Program
+    {
+        public static  string _input1 = "class P {f() : Int { 1 };};";
+        public static string _input2 = "x <- 5 + 3;";
+        public static string _input3 = "x <- 2 + 3 * 3 / 5;";
+
+        static void Main(string[] args)
+        {
+            LexerTest();
+
+            ParserTest();
+        }
+
+        static void LexerTest()
+        {
+            //string input = "class Cons inherits List { xcar: Int;   xcdr: List; isNil() : Bool { false };   init(hd: Int, tl: List) : Cons { { xcar < -hd;   xcdr < -tl;    self;    }}};";
+
+            AntlrInputStream inputStream = new AntlrInputStream(_input3);
+            CoolLexer lex = new CoolLexer(inputStream);
+
+            List<IToken> itl = (List<IToken>)lex.GetAllTokens();
+            foreach (var item in itl)
+            {
+                Console.Write(item.ToString() + " -> ");
+                string tokenname = lex.Vocabulary.GetDisplayName(item.Type);
+
+                // string tokenname = lex.Vocabulary.GetLiteralName(item.Type);
+                // if (tokenname == null) tokenname = lex.Vocabulary.GetSymbolicName(item.Type);
+                Console.WriteLine(tokenname);
+            }
+        }
+
+        static void ParserTest()
+        {
+            //string input = "john says: hello @michael this will not work\n";
+
+            AntlrInputStream inputStream = new AntlrInputStream(_input3);
+            CoolLexer lex = new CoolLexer(inputStream);
+
+            CommonTokenStream commonTokenStream = new CommonTokenStream(lex);
+
+            CoolParser parser = new CoolParser(commonTokenStream);
+
+            //CoolParser.ClassDefineContext ctx1 = parser.classDefine();
+            //Console.WriteLine(ctx1.ToStringTree());
+            CoolParser.ExpressionContext ctx2 = parser.expression();
+            Console.WriteLine(ctx2.ToStringTree());
+            //CoolParser.MethodContext ctx3 = parser.method();
+            //Console.WriteLine(ctx3.ToStringTree());
+
+            CoolVisitor1 v = new CoolVisitor1();
+            var res = v.Visit(ctx2);
+        }
+    }
+}
